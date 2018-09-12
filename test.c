f7f83b7e7ebfbd5eaec9d54ddedf52d6104d2ad1
rihjeo <951010v@hanmail.net>
2018-09-12 19:46:27 +0900

    final edit

--- test.c
+++ test.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-<<<<<<< HEAD
-<<<<<<< HEAD
 	printf("Helloadsf");
 	printf("master");
 	printf("Helloadsadfsf");
 	printf("hard");
-=======
 	printf("Helloadsadfsf");//dfadfs
->>>>>>> hyeong
 	return 0;
 }
